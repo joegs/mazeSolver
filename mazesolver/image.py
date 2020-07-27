ae5f6f44e65a4b907c23a0a0a17c583a92f7605c
Joel Gomez Sanchez <joelgomezsanchez00@gmail.com>
2020-07-27 15:02:18 -0500

    Scale image upon loading
    By scaling the image to a known size when loading it,
    the speed at which the maze is solved is increased.

--- mazesolver/image.py
+++ mazesolver/image.py
@@ -6,18 +6,32 @@ import time
 
 
 class MazeImage:
+    DEFAULT_SIZE = (300, 300)
+
     def __init__(self, image_path: str):
         self.pixels: np.ndarray
         self.bw_pixels: np.ndarray
         self.result: np.ndarray
         self.load_image(image_path)
 
+    def get_scaled_size(self):
+        height, width, _ = self.pixels.shape
+        ratio = width / height
+        if width > height:
+            scaled_width = self.DEFAULT_SIZE[0]
+            scaled_height = int(scaled_width / ratio)
+        else:
+            scaled_height = self.DEFAULT_SIZE[1]
+            scaled_width = int(scaled_height * ratio)
+        size = (scaled_width, scaled_height)
+        return size
+
     def load_image(self, image_path: str):
-        pixels = cv2.imread(image_path, cv2.IMREAD_COLOR)
-        bw_pixels = cv2.cvtColor(pixels, cv2.COLOR_BGR2GRAY)
+        self.pixels = cv2.imread(image_path, cv2.IMREAD_COLOR)
+        scaled_size = self.get_scaled_size()
+        self.pixels = cv2.resize(self.pixels, scaled_size)
+        bw_pixels = cv2.cvtColor(self.pixels, cv2.COLOR_BGR2GRAY)
         _, bw_pixels = cv2.threshold(bw_pixels, 150, 255, cv2.THRESH_BINARY)
-        rgb_pixels = cv2.cvtColor(pixels, cv2.COLOR_BGR2RGB)
-        self.pixels = rgb_pixels
         self.bw_pixels = bw_pixels
         self.result = np.copy(self.pixels)
 
